160d1c6fd417f58892a24f132e321270bc0edb36
UranKhatolaaa <61351435+UranKhatolaaa@users.noreply.github.com>
2021-05-30 17:40:05 +0300

    Update process.py

--- yolov4-tiny/process.py
+++ yolov4-tiny/process.py
@@ -5,7 +5,12 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 
 print(current_dir)
 
-current_dir = 'data/obj'
+#path to one folder that has both images and labels
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--imgdir", type=str, required=True, help="File containing the image paths")
+args = parser.parse_args()
+
+current_dir = args.imgdir
 
 # Percentage of images to be used for the test set
 percentage_test = 20;
@@ -22,7 +27,7 @@ for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.jpg")):
 
     if counter == index_test:
         counter = 1
-        file_test.write("data/obj" + "/" + title + '.jpg' + "\n")
+        file_test.write(current_dir + "/" + title + '.jpg' + "\n")
     else:
-        file_train.write("data/obj" + "/" + title + '.jpg' + "\n")
+        file_train.write(current_dir + "/" + title + '.jpg' + "\n")
         counter = counter + 1
